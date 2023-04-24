47db7106e100dfa45fedc358e646361319b4a357
Tor Inge Tungen Andersen <toringe@sigruns-mbp.kantega.lan>
2023-04-24 14:10:02 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgpic("road.gif")
+
+y_positions = [-260, -172, -85, 2, 85, 172, 260]
+colors = ["white", "red", "orange", "pink", "tomato", "dodgerblue", "yellow"]
+all_turtle = []
+user_bet = screen.textinput("Enter your bet", prompt="Which turtle (color): ")
+
+for index in range(0, 7):
+    new_tur = Turtle(shape="turtle")
+    new_tur.shapesize(2)
+    new_tur.speed("fastest")
+    new_tur.penup()
+    new_tur.goto(x=-350, y=y_positions[index])
+    new_tur.color(colors[index])
+    all_turtle.append(new_tur)
+
+is_on = True
+
+while is_on:
+    for turtle in all_turtle:
+        if turtle.xcor() > 330:
+            is_on = False
+            winner = turtle.pencolor()
+            if winner == user_bet:
+                print(f"You won! {winner} turtle is the winner")
+            else:
+                print(f"You lost! The {winner} turtle is the winner")
+        random_pace = random.randint(0, 7)
+        turtle.forward(random_pace)
+
+
+screen.exitonclick()
